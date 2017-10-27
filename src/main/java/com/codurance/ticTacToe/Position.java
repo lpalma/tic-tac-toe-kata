d61274a93fb2ac7ad43f0e85689e11820f567eeb
Luciano Palma <lucianopalma90@gmail.com>
2017-10-27 10:43:35 +0100

    win for player X on first row

---
+++ src/main/java/com/codurance/ticTacToe/Position.java
@@ -0,0 +1,6 @@
+package com.codurance.ticTacToe;
+
+public class Position {
+    public Position(int row, int column) {
+    }
+}
---
+++ src/main/java/com/codurance/ticTacToe/TicTacToeBoard.java
@@ -0,0 +1,12 @@
+package com.codurance.ticTacToe;
+
+import java.util.List;
+
+public class TicTacToeBoard {
+    public TicTacToeBoard() {
+    }
+
+    public String play(List<Position> positions) {
+        return "X";
+    }
+}
