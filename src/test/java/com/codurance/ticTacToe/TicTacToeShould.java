0a1b9f4eac1290714afe34aad526fa4abd3376e0
Luciano Palma <lucianopalma90@gmail.com>
2017-10-29 19:29:14 +0000

    parameterise victories for X

--- src/test/java/com/codurance/ticTacToe/TicTacToeShould.java
+++ src/test/java/com/codurance/ticTacToe/TicTacToeShould.java
@@ -3,17 +3,35 @@ package com.codurance.ticTacToe;
 import com.codurance.ticTacToe.board.Board;
 import com.codurance.ticTacToe.gameState.GameState;
 import com.codurance.ticTacToe.gameState.PlayerXTurn;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.Optional;
 
 import static com.codurance.ticTacToe.Result.O_WON;
 import static com.codurance.ticTacToe.Result.X_WON;
 import static com.codurance.ticTacToe.Square.*;
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
+@RunWith(JUnitParamsRunner.class)
 public class TicTacToeShould {
+
+    private Object xWins() {
+        return asList(
+                asList(TOP_LEFT, MIDDLE_LEFT, TOP_CENTER, MIDDLE_CENTER, TOP_RIGHT),
+                asList(MIDDLE_LEFT, TOP_LEFT, MIDDLE_CENTER, TOP_CENTER, MIDDLE_RIGHT),
+                asList(BOTTOM_LEFT, TOP_LEFT, BOTTOM_CENTER, TOP_CENTER, BOTTOM_RIGHT),
+                asList(TOP_LEFT, TOP_CENTER, MIDDLE_LEFT, MIDDLE_CENTER, BOTTOM_LEFT),
+                asList(TOP_CENTER, TOP_LEFT, MIDDLE_CENTER, MIDDLE_LEFT, BOTTOM_CENTER),
+                asList(TOP_RIGHT, TOP_CENTER, MIDDLE_RIGHT, MIDDLE_CENTER, BOTTOM_RIGHT),
+                asList(TOP_LEFT, TOP_CENTER, MIDDLE_CENTER, TOP_RIGHT, BOTTOM_RIGHT),
+                asList(TOP_RIGHT, TOP_CENTER, MIDDLE_CENTER, TOP_LEFT, BOTTOM_LEFT)
+        );
+    }
 
     @Test
     public void
@@ -22,14 +40,15 @@ public class TicTacToeShould {
     }
 
     @Test
+    @Parameters(method = "xWins")
     public void
-    result_in_victory_to_X_when_X_wins() throws InvalidMoveException, GameFinishedException {
+    result_in_victory_to_X_when_X_wins(Square[] squares) throws InvalidMoveException, GameFinishedException {
         GameState game = TicTacToe.newGame()
-                .playOn(TOP_LEFT)
-                .playOn(MIDDLE_LEFT)
-                .playOn(TOP_CENTER)
-                .playOn(MIDDLE_CENTER)
-                .playOn(TOP_RIGHT);
+                .playOn(squares[0])
+                .playOn(squares[1])
+                .playOn(squares[2])
+                .playOn(squares[3])
+                .playOn(squares[4]);
 
         assertThat(game.result(), equalTo(Optional.of(X_WON)));
     }
